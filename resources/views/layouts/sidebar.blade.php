6db4204afbf3e2e5e9428591088669e64583f509
Raje-braincave <rajeswari@braincavesoft.com>
2023-04-26 18:36:13 +0530

    absolut project all code

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,49 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+    <ul class="nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">
+          <i class="mdi mdi-grid-large menu-icon"></i>
+          <span class="menu-title">Dashboard</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="customer-managment.html">
+          <i class="mdi mdi-account-multiple menu-icon"></i>
+         <span class="menu-title">CRM</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="leads.html">
+          <i class="mdi mdi-account menu-icon"></i>
+          <span class="menu-title">Leads</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="finance.html">
+          <i class="mdi mdi-cash-multiple menu-icon"></i>
+          <span class="menu-title">Finance</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="service-managment.html">
+          <i class="mdi mdi mdi-settings menu-icon"></i>
+          <span class="menu-title">Service Management</span>
+        </a>
+      </li>
+      
+      
+      <li class="nav-item">
+        <a class="nav-link" href="calendar.html">
+          <i class="mdi mdi-calendar-multiple-check menu-icon"></i>
+          <span class="menu-title">Schedule</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">
+          <i class="mdi mdi mdi-format-list-bulleted menu-icon"></i>
+          <span class="menu-title">HRMS</span>
+        </a>
+      </li>
+     
+    </ul>
+  </nav>
